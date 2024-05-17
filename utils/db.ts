88517d82ba7a4df0e1a098681ec5bc94e5f84683
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-05-16 18:04:32 -0600

    tryna modify the logging of queries to not reveal sensitive infos, but having a bit of errors

--- utils/db.ts
+++ utils/db.ts
@@ -1,16 +1,36 @@
 // connection to the database using prisma
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
+
+// create a global object to store the prisma client
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
+// create a new prisma client if it doesn't exist
 export const prisma = 
     globalForPrisma.prisma ?? 
     new PrismaClient({
-        log: ['query'], // log queries for debugging
+        log: [{
+            emit: 'event',
+            level: 'query',
+        }],
     })
+
+//define a function to sanitize prisma queries
+function sanitizeQuery(query: string | never): string {
+    // replace any sensitive information in the query string with '*****'
+    return query.replace(/(password|token|refreshToken|authorization)/gi, '*****');
+}
+
+
+// event listener for the 'query' event
+prisma.$on('query', (e: any) => {
+    const event = e as Prisma.QueryEvent;
+    const { query } = event;
+    const safeQuery = sanitizeQuery(query as string);
+    console.log(`Query: ${safeQuery}`);
 
 
 // prevent too many connections

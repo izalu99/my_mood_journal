eb2a5944e91357957a99ef174cbc8b510949fad0
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-06-05 18:30:23 -0600

    history chart created and added to history page.

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,20 +1,36 @@
 import { prisma } from '@/utils/db';
 import { getUserByClerkID } from '@/utils/auth';
+import HistoryChart from '@/components/HistoryChart';
 
 const getData = async () => {
     const user = await getUserByClerkID()
     const analyses = await prisma.analysis.findMany({
         where:{
-            
-        }
+            userId:user.id,
+        },
+        orderBy:{
+            createdAt:'asc',
+        },
     })
+
+    const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+    const average = Math.round(sum / analyses.length)
+
+    return {analyses, average }
 }
 
 
-const History = () => {
+const History = async () => {
+    const { average, analyses } = await getData()
     return (
-        <div>
+        <div className='w-full h-full'>
             <h1>History</h1>
+            <div>
+                `Average Sentiment: ${average}`
+            </div>
+            <div className='w-full h-full'>
+                <HistoryChart data={analyses}/>
+            </div>
         </div>
     );
 }
---
+++ components/HistoryChart.tsx
@@ -0,0 +1,58 @@
+
+'use client'
+
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+
+
+
+const CustomTooltip = ({ active, payload, label }) => {
+
+    const dateLabel = new Date(label).toLocaleString('en-CA',{
+        weekday:'long',
+        month:'short',
+        year:'numeric',
+        day:'numeric',
+        hour:'numeric',
+        minute:'numeric',
+    })
+
+    if (active) {
+        const analysis = payload[0].payload
+        return (
+            <div className='p-8 custom-tooltip bg-black/5 shadow-md border-white/10 rounded-lg backdrop-blur-md relative'>
+                <div
+                className='absolute left-2 top-2 w-2 h-2 rounded-full'
+                style={{background: analysis.color}}
+                ></div>
+                <p className='label text-sm text-white/30'>{dateLabel}</p>
+                <p className='intro text-xl uppercase'>{analysis.mood}</p>
+            </div>
+      );
+    }
+  
+    return null;
+  }
+
+
+
+const HistoryChart = ({ data }) => {
+
+    return (
+        <ResponsiveContainer width="100%" height= "100%">
+            <LineChart width={300} height={100} data={data}>
+                <Line 
+                type="monotone"
+                dataKey="sentimentScore" 
+                stroke="#8884d8" 
+                strokeWidth={2}
+                activeDot={{r: 8}}
+                />
+                <XAxis dataKey="createdAt" />
+                <Tooltip content={<CustomTooltip />}/>
+            </LineChart>
+        </ResponsiveContainer>
+    )
+
+}
+
+export default HistoryChart;
